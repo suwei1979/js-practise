3e676ae44855da6a05b96cbbce9c8bd23f5d029f
suwei <suwei1979@139.com>
2019-01-21 15:31:21 +0800

    js practise

--- myapp/server.js
+++ myapp/server.js
@@ -2,19 +2,30 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-
+app.all('*', function (request, response, next) {
+    console.log('In comes a ' + request.method + ' to ' + request.url);
+    response.writeHead(200, {"Content-Type": 'text/plain'});
+    next();
+})
 app.get('/', function (request, response) {
-    response.send('Ritsu Yan');
+    response.write('Ritsu Yan');
+
 });
 
 app.get('/about', function(req, resp) {
-    resp.send('the node course');
+    resp.write('the node course');
 });
 
-app.use(function (req, resp, next) {
+app.use(function (req, resp) {
     resp.status(404);
-    resp.send('404 - Not Found');
+    resp.end('404 - Not Found');
+
 });
+
+app.use(function (request, response) {
+    response.status(200);
+    console.log('a request processed, status is ' + response.getHeaderNames());
+})
 
 app.listen(app.get('port'), function () {
     console.log('express started on port %d', app.get('port'));
